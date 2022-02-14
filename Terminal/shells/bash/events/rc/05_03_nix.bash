c93c5121460664ed2edfa71c5291bd888d1c769c
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-14 10:47:02 -0600

    improve nix

--- Terminal/shells/bash/events/rc/05_03_nix.bash
+++ Terminal/shells/bash/events/rc/05_03_nix.bash
@@ -1,6 +1,27 @@
-# check if file exists
-if [[ -f "/etc/profile.d/nix.sh" ]]
+export NIXPKGS_ALLOW_UNFREE=1
+
+# do the sourcing check if file exists
+if [ -f "/etc/profile.d/nix.sh" ]
 then
     . /etc/profile.d/nix.sh
+else
+    # 
+    # manual fallback
+    # 
+    
+    # per-user 
+    __temp_var__user_nix_bin="/nix/var/nix/profiles/per-user/$(whoami)/profile/bin/"
+    if [ -d "$__temp_var__user_nix_bin" ]
+    then
+        export PATH="$PATH:$__temp_var__user_nix_bin"
+    fi
+    unset __temp_var__user_nix_bin
+    
+    # default
+    __temp_var__default_nix_bin="/nix/var/nix/profiles/default/bin/nix"
+    if [ -d "$__temp_var__default_nix_bin" ]
+    then
+        export PATH="$PATH:$__temp_var__default_nix_bin"
+    fi
+    unset __temp_var__default_nix_bin
 fi
-export NIXPKGS_ALLOW_UNFREE=1
