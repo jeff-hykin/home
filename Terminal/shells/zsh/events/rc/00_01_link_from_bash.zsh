3d6e03082884185c9d113c124ce8fc66c9dcc6f8
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-14 11:33:12 -0600

    -

--- Terminal/shells/zsh/events/rc/00_01_link_from_bash.zsh
+++ Terminal/shells/zsh/events/rc/00_01_link_from_bash.zsh
@@ -1,16 +1,15 @@
-{
-    cd "$SHELL_STANDARD_ENV_SOURCE/shells/"
-    ln -sf ../../../bash/events/rc/01_00_linux_keybindings.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/01_11_set_editor_var.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/02_00_universal_tools.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/02_01_mac_tools.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/02_04_wasmer.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/03_01_git_tools.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/03_03_add_local_bin_to_path.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/04_04_docker_tools.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/05_02_python3.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/05_03_nix.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/05_04_rust.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/06_01_ld_path.bash ./zsh/events/rc/
-    ln -sf ../../../bash/events/rc/06_01_setup_cuda.bash ./zsh/events/rc/
-}
+cd "$SHELL_STANDARD_ENV_SOURCE/shells/"
+ln -sf ../../../bash/events/rc/01_00_linux_keybindings.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/01_11_set_editor_var.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/02_00_universal_tools.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/02_01_mac_tools.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/02_04_wasmer.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/03_01_git_tools.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/03_03_add_local_bin_to_path.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/04_04_docker_tools.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/05_02_python3.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/05_03_nix.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/05_04_rust.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/06_01_ld_path.bash ./zsh/events/rc/
+ln -sf ../../../bash/events/rc/06_01_setup_cuda.bash ./zsh/events/rc/
+cd -
