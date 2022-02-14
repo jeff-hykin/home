3ee85a5abadd17b0529930c7e92956da49650912
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-14 09:33:34 -0600

    automate autosuggest

--- Terminal/shells/zsh/rc/01_04_zsh_autosuggest.zsh
+++ Terminal/shells/zsh/rc/01_04_zsh_autosuggest.zsh
@@ -1,10 +1,4 @@
-if [ -f "$HOME/repos/standard_install/linked/zsh-autosuggestions/zsh-autosuggestions.zsh" ]
+if [ -f "$SHELL_STANDARD_ENV_SOURCE/plugins/zsh-autosuggestions" ]
 then
-    source "$HOME/repos/standard_install/linked/zsh-autosuggestions/zsh-autosuggestions.zsh"
+    . "$SHELL_STANDARD_ENV_SOURCE/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 fi
-if [ -f "$HOME/repos/zsh-autosuggestions/zsh-autosuggestions.zsh" ]
-then
-    source "$HOME/repos/zsh-autosuggestions/zsh-autosuggestions.zsh"
-fi
-# skip_global_compinit=1
-# source "$HOME/repos/standard_install/linked/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
