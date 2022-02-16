a360a7a818a7ac7647dccbb8b25bc637d59252ee
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-16 07:04:56 -0600

    -

--- Terminal/shells/zsh/events/rc/01_04_zsh_autosuggest.zsh
+++ Terminal/shells/zsh/events/rc/01_04_zsh_autosuggest.zsh
@@ -1,4 +1,4 @@
-if [ -f "$SHELL_STANDARD_ENV_SOURCE/plugins/zsh-autosuggestions" ]
+if [ -f "$SHELL_STANDARD_ENV_SOURCE/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]
 then
     . "$SHELL_STANDARD_ENV_SOURCE/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 fi
