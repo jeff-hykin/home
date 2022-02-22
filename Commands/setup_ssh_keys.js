080ca0f86ff6ff3358f1506f784278be0806e0a4
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-21 19:39:35 -0600

    first part of ssh working

--- Commands/setup_ssh_keys.js
+++ Commands/setup_ssh_keys.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-all
 const { run, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, zipInto, mergeInto, returnAsString, } = await import(`https://deno.land/x/sprinter@0.3.1/index.js`)
-const { FileSystem, Console } = await import(`https://deno.land/x/file_system_js@0.0.18/main/deno.js`)
+const { FileSystem, Console } = await import(`https://deno.land/x/file_system_js@0.0.21/main/deno.js`)
 const { vibrance } = (await import('https://cdn.skypack.dev/vibrance@v0.1.33')).default
 
 const sshFolder = `${FileSystem.home}/.ssh/`
@@ -25,8 +25,9 @@ if (await Console.askFor.yesNo("Is this script running on your machine right now
     await ensurePrivatePublicRsaKeyExists()
     console.log("Now run this same script on the remote server (on your account)")
     console.log("Paste the value below when it asks for your public key")
+    console.log("(and FYI you can show this to anyone, its not private)")
     console.log()
-    vibrance.bgBlack.blue(FileSystem.read(defaultPublicKeyPath)).log()
+    vibrance.bgBlack.blue(await FileSystem.read(defaultPublicKeyPath)).log()
     console.log()
     console.log()
 } else {
@@ -49,14 +50,14 @@ function escapeShellArgument(string) {
 }
 
 async function ensurePrivatePublicRsaKeyExists() {
-    let privateKey = FileSystem.info(defaultPrivateKeyPath)
-    let publicKey = FileSystem.info(defaultPublicKeyPath)
+    let privateKey = await FileSystem.info(defaultPrivateKeyPath)
+    let publicKey = await FileSystem.info(defaultPublicKeyPath)
     while (!privateKey.isFile || !publicKey.isFile) {
         console.log("\nLooks like your private key doesn't exist so lets make one")
         vibrance.bgBlack.white("    running: ").bgBlack.blue("ssh-keygen -t rsa").log()
         await run("ssh-keygen", "-t", "rsa", "-f", defaultPrivateKeyPath)
-        privateKey = FileSystem.info(defaultPrivateKeyPath)
-        publicKey = FileSystem.info(defaultPublicKeyPath)
+        privateKey = await FileSystem.info(defaultPrivateKeyPath)
+        publicKey = await FileSystem.info(defaultPublicKeyPath)
     }
 }
 
@@ -66,7 +67,7 @@ async function getFingerprint(path=defaultPublicKeyPath) {
 
 async function verifyAndSetupSshFolderStructure() {
     await FileSystem.ensureIsFolder(sshFolder)
-    const authInfo = FileSystem.info(authorizedKeysPath)
+    const authInfo = await FileSystem.info(authorizedKeysPath)
     if (!authInfo.exists) {
         await Deno.writeTextFile(authorizedKeysPath, "")
     }
@@ -74,7 +75,7 @@ async function verifyAndSetupSshFolderStructure() {
 
     // SSH folder on the server needs 700 permissions: chmod 700 /home/$USER/.ssh
     await FileSystem.addPermissions({
-        path: privateKeysFolder,
+        path: sshFolder,
         permissions: {
             owner:{
                 canRead: true,
@@ -96,7 +97,7 @@ async function verifyAndSetupSshFolderStructure() {
     })
     // authorized_keys file needs 644 permissions: chmod 644 /home/$USER/.ssh/authorized_keys
     await FileSystem.addPermissions({
-        path: privateKeysFolder,
+        path: authorizedKeysPath,
         permissions: {
             owner:{
                 canRead: true,
@@ -118,9 +119,11 @@ async function verifyAndSetupSshFolderStructure() {
     })
     // Make sure that user owns the files/folders and not root: chown user:user authorized_keys and chown user:user /home/$USER/.ssh
     const username = await run("whoami", Stdout(returnAsString))
-    const fileOwner = await run("stat", "-c", "%U", sshFolder)
+    const fileOwner = await run("stat", "-c", "%U", sshFolder, Stdout(returnAsString))
     if (username != fileOwner) {
         console.log(`\nFor some reason you're not the owner of your own home/.ssh/ folder (strange)`)
+        console.log(`    your  username: ${username}`)
+        console.log(`    owner username: ${fileOwner}`)
         console.log(`Lets change that:\n`)
         await run("sudo", "chown", username, sshFolder)
     }
