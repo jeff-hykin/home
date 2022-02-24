1c05e46e31ccdca942770cf39fb1bf365f4fe425
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-24 16:59:07 -0600

    debugging compute server

--- Terminal/shells/zsh/events/rc/01_07_spaceship_prompt.zsh
+++ Terminal/shells/zsh/events/rc/01_07_spaceship_prompt.zsh
@@ -4,39 +4,39 @@ SPACESHIP_VENV_GENERIC_NAMES="."
 SPACESHIP_VENV_COLOR="green"
 SPACESHIP_NODE_COLOR="yellow"
 SPACESHIP_PROMPT_ORDER=(
-    time          # Time stampts section
-    user          # Username section
-    dir           # Current directory section
-    host          # Hostname section
-    git           # Git section (git_branch + git_status)
-    hg            # Mercurial section (hg_branch  + hg_status)
-    package       # Package version
-    node          # Node.js section
-    ruby          # Ruby section
-    elm           # Elm section
-    elixir        # Elixir section
-    xcode         # Xcode section
-    swift         # Swift section
-    golang        # Go section
-    php           # PHP section
-    rust          # Rust section
-    haskell       # Haskell Stack section
-    julia         # Julia section
-    docker        # Docker section
-    aws           # Amazon Web Services section
-    venv          # virtualenv section
-    conda         # conda virtualenv section
-    pyenv         # Pyenv section
-    dotnet        # .NET section
-    ember         # Ember.js section
-    terraform     # Terraform workspace section
-    exec_time     # Execution time
-    line_sep      # Line break
-    battery       # Battery level and status
-    vi_mode       # Vi-mode indicator
-    jobs          # Background jobs indicator
-    exit_code     # Exit code section
-    char          # Prompt character
+    # time          # Time stampts section
+    # user          # Username section
+    # dir           # Current directory section
+    # host          # Hostname section
+    # git           # Git section (git_branch + git_status)
+    # hg            # Mercurial section (hg_branch  + hg_status)
+    # package       # Package version
+    # node          # Node.js section
+    # ruby          # Ruby section
+    # elm           # Elm section
+    # elixir        # Elixir section
+    # xcode         # Xcode section
+    # swift         # Swift section
+    # golang        # Go section
+    # php           # PHP section
+    # rust          # Rust section
+    # haskell       # Haskell Stack section
+    # julia         # Julia section
+    # docker        # Docker section
+    # aws           # Amazon Web Services section
+    # venv          # virtualenv section
+    # conda         # conda virtualenv section
+    # pyenv         # Pyenv section
+    # dotnet        # .NET section
+    # ember         # Ember.js section
+    # terraform     # Terraform workspace section
+    # exec_time     # Execution time
+    # line_sep      # Line break
+    # battery       # Battery level and status
+    # vi_mode       # Vi-mode indicator
+    # jobs          # Background jobs indicator
+    # exit_code     # Exit code section
+    # char          # Prompt character
     # for some reasons these cause errors inside vs code: 
     # ibmcloud      # IBM Cloud section
     # gradle        # Gradle section
