05cfa765522b48aad35e6d8c794a8b4d28525953
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-08 17:24:04 -0500

    improve git tools

--- Terminal/shells/bash/events/rc/03_01_git_tools.bash
+++ Terminal/shells/bash/events/rc/03_01_git_tools.bash
@@ -11,12 +11,16 @@ git_checkout () {
         git switch "$@"
         return 
     }
-    printf '%s' "$__temp_var__branches" | grep "$2" 2>/dev/null 1>/dev/null && {
+    # if second arg exists
+    if [ -n "$2" ]
+    then
+        printf '%s' "$__temp_var__branches" | grep "$2" 2>/dev/null 1>/dev/null && {
+            unset __temp_var__branches
+            git switch "$@"
+            return
+        }
         unset __temp_var__branches
-        git switch "$@"
-        return
-    }
-    unset __temp_var__branches
+    fi
     # 
     # otherwise use checkout
     # 
@@ -35,20 +39,6 @@ git_log () {
 git_current_commit_hash () {
     # https://stackoverflow.com/questions/949314/how-to-retrieve-the-hash-for-the-current-commit-in-git
     git rev-parse HEAD
-}
-
-git_oldest_commit_hash () {
-    git log --reverse --oneline | head -n1 | sed -e 's/ .*//' 
-}
-
-git_squash_to () {
-    commit_hash="$1"
-    git reset --soft "$commit_hash"
-    git add -A
-}
-
-git_squash_all () {
-    git reset --soft $(git_oldest_commit_hash)
 }
 
 # 
@@ -106,6 +96,34 @@ git_keep_theirs () { # git keep theirs
     git checkout --theirs .
     git add -u
     git commit -m "_Accepting all incoming changes $@"
+}
+
+git_add_upstream () {
+    remote_name="$1"
+    remote_url="$2"
+    if [ -z "$remote_name" ]
+    then
+        echo "what should the upstream source be called?"
+        read remote_name
+    fi
+    if [ -z "$remote_url" ]
+    then
+        echo "what is the url to the upstream source?"
+        read remote_url
+    fi
+    
+    git remote add "$remote_name" "$remote_url"
+}
+
+git_change_origin () {
+    remote_url="$1"
+    if [ -z "$remote_url" ]
+    then
+        echo "what is the url to the upstream source?"
+        read remote_url
+    fi
+    # change origin
+    git remote set-url "origin" "$remote_url"
 }
 
 # 
@@ -420,5 +438,7 @@ alias gb="git branch -a"
 alias gnb="git_new_branch"
 alias gd="git_delete_changes"
 alias gcp="git add -A;git stash"
+alias gct="git add -A;git stash"
 alias gpst="git stash pop;git add -A"
 alias gundo="git reset --soft HEAD~1"
+alias gurl="git_url_of_origin"
