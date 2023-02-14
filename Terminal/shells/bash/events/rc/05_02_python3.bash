768d4250a21f1f9c344334544bafcb53d5cbba86
Jeff Hykin <jeff.hykin@gmail.com>
2023-02-14 08:43:54 -0600

    -

--- Terminal/shells/bash/events/rc/05_02_python3.bash
+++ Terminal/shells/bash/events/rc/05_02_python3.bash
@@ -7,7 +7,7 @@ pip () {
 }
 
 __temp_var__startup_file="$HOME/.cache/python_start.py"
-mkdir -p "$__temp_var__startup_file"
+mkdir -p "$(dirname "$__temp_var__startup_file")"
 echo 'import blissful_basics as bb;import numpy;import numpy as np;import torch;import os;import sys;import random;import time;from statistics import mean as average;A=numpy.array;T=torch.tensor' > "$__temp_var__startup_file"
 alias p="PYTHONSTARTUP=""$__temp_var__startup_file"" python "
 unset __temp_var__startup_file
