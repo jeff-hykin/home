e8e2d04b66795d4e045c5f3a353e32dbda9c09ba
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-09 13:18:50 -0600

    improve fd

--- Terminal/shells/zsh/events/rc/07_02_wrap_fd.zsh
+++ Terminal/shells/zsh/events/rc/07_02_wrap_fd.zsh
@@ -1,4 +1,18 @@
 __path_to_fd="$(which fd)"
 fd () {
-    "$__path_to_fd" --hidden --no-ignore -p "$@"
+    # for each argument (in a argument-might-have-spaces friendly way)
+    for arg in "$@"; do
+        # FIXME: this is imperfect because these could appear after a "--"
+        if [ "--exact-depth" = "$arg" ] || [ "--max-depth" = "$arg" ] || [ "--min-depth" = "$arg" ] || [ "-d" = "$arg" ]
+        then
+            return "$__path_to_fd" --hidden --no-ignore -p "$@"
+        fi
+    done
+    
+    # poor mans breath-first-search since fd doesn't support it yet 
+    # https://github.com/sharkdp/fd/issues/734
+    index=-1 ; while [ "$index" -lt 256 ]; do
+        index=$((index+1))
+        "$__path_to_fd" --exact-depth $index --hidden --no-ignore -p "$@"
+    done
 }
