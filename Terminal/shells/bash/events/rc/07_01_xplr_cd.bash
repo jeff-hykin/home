257e82d33144050e53bd8900bf499dc8dae9d1e2
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-05 11:48:18 -0600

    improve the x command

--- Terminal/shells/bash/events/rc/07_01_xplr_cd.bash
+++ Terminal/shells/bash/events/rc/07_01_xplr_cd.bash
@@ -1,2 +1,13 @@
 alias c='cd "$(xplr --print-pwd-as-result)"'
-alias x='cd "$(xplr --print-pwd-as-result)"'
+
+x () {
+    if [ -z "$1" ]
+    then
+        cd "$(xplr --print-pwd-as-result)"
+    else
+        local current_dir="$PWD"
+        cd "$1"
+        xplr --print-pwd-as-result
+        cd "$current_dir"
+    fi
+}
