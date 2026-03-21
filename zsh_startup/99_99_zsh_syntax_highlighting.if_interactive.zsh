c14ed6c3153d5d71b1a4964b8123dc7acd619da7
Jeff Hykin <jeff.hykin@gmail.com>
2026-03-21 13:42:29 -0700

    zsh fix linux

--- zsh_startup/99_99_zsh_syntax_highlighting.if_interactive.zsh
+++ zsh_startup/99_99_zsh_syntax_highlighting.if_interactive.zsh
@@ -1,6 +1,6 @@
 # zsh-syntax-highlighting — must be sourced after all other plugins and shell setup
-if [ -f "$HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-    . "$HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+if [ -f "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
+    . "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 elif [ -f /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
     . /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 elif [ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
